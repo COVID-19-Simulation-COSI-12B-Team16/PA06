81e36e5c9e545405a6155e908313b7263f5a0978
He Zhang <852072754@qq.com>
2020-04-19 18:05:23 -0400

    randompick and compute variance

--- src/KMean.java
+++ src/KMean.java
@@ -1,12 +1,17 @@
+import java.util.*;
+
+class Sample{}
+class Cluster{}
+
 public class KMean {
     private double prevVariance;
     private List<Cluster> clusters;
     private int K;
     private double variance;
-    private List<Sample> sample;
+    private List<Sample> samples;
 
     KMean(List<Sample> _samples, int _K){
-        sample = _samples;
+        samples = _samples;
         K = _K;
     }
 
@@ -15,9 +20,34 @@ public class KMean {
     private void assignSamples(){}
 
     // compute current sum of variance
-    private double computeVariance(){ return .0;}
+    private double computeVariance(){
+        double sum = 0;
+        for(int i = 0; i < K; i++) {
+            Cluster cur = clusters.get(i);
+            for(int j = 0; j < cur.getSamples().size(); j++) {
+                sum += getDistance(cur.getCenter(), cur.getSamples().get(j));
+            }
+        }
 
-    private List<Sample> randomPickSample(){ return null; }
+        return sum;
+    }
+
+    private void randomPickSample(){
+        Random random = new Random();
+        List<Sample> res = new ArrayList<>();
+        Set<Integer> set = new HashSet<>();
+        for(int i = 0; i < K; i++) {
+            int index = random.nextInt(samples.size());
+            while(set.contains(index)) {
+                index = random.nextInt(samples.size());
+            }
+            set.add(index);
+            Cluster c = new Cluster(samples.get(index)); // the constructor of Cluster must take a sample variable
+            clusters.add(c);
+        }
+
+
+    }
 
 
     double getVariance(){ return variance;}
