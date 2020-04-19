c751a5c4f860cc8f9b1b011530cb98460d6149f0
yirunzhou <yirunzhou@brandeis.edu>
2020-04-19 18:09:58 -0400

    KMean.optimze() seperate to smaller methods

--- src/KMean.java
+++ src/KMean.java
@@ -1,8 +1,8 @@
 public class KMean {
-    private double prevVariance;
+    private Double prevVariance;
     private List<Cluster> clusters;
     private int K;
-    private double variance;
+    private Double variance;
     private List<Sample> samples;
 
     KMean(List<Sample> _samples, int _K){
@@ -13,7 +13,30 @@ public class KMean {
     /**
      * Iterate a number of times until variance does not change a lot
      */
-    private void optimize(){}
+    private void optimize(){
+        randomPickSample();
+        assignSamples();
+        prevVariance = computeVariance();
+        while(variance == None){
+            generateNewClusters();
+            assignSamples();
+            variance = computeVariance();
+            if(terminationCondition()) break;
+        }
+    }
+
+    /**
+     * Compute centers of clusters then create new empty clusters with these clusters
+     */
+    private void generateNewClusters(){
+
+    }
+
+    /**
+     * Compare prevVariance and variance, if difference small enough, then return true
+     * @return
+     */
+    private boolean terminationCondition(){ return false;}
 
     /**
      * Assign all samples to current clusters according to distances
