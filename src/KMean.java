8dd48574aa98b3aac362f2fb93ae4119d861b0a6
yirunzhou <yirunzhou@brandeis.edu>
2020-04-19 17:23:27 -0400

    KMean empty methods

--- src/KMean.java
+++ src/KMean.java
@@ -1,2 +1,29 @@
 public class KMean {
+    private double prevVariance;
+    private List<Cluster> clusters;
+    private int K;
+    private double variance;
+    private List<Sample> sample;
+
+    KMean(List<Sample> _samples, int _K){
+        sample = _samples;
+        K = _K;
+    }
+
+    private void optimize(){}
+
+    private void assignSamples(){}
+
+    // compute current sum of variance
+    private double computeVariance(){ return .0;}
+
+    private List<Sample> randomPickSample(){ return null; }
+
+
+    double getVariance(){ return variance;}
+
+    List<Cluster> getResult(){ return null;}
+
+    // write results to cvs
+    void writeToFile(){}
 }
